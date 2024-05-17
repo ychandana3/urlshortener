86a192ae99f64bf26580e1093e3c14d9387f232f
Yamini Chandana <yaminichandana9@gmail.com>
2024-05-17 23:13:38 +0530

    Added styles in App using styled components

--- views/src/App.js
+++ views/src/App.js
@@ -1,18 +1,61 @@
 
 import { createBrowserRouter, NavLink } from "react-router-dom";
 import ShortUrl from "./ShortUrl";
+import styled from 'styled-components';
+
+const AppContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  
+  height: 100vh;
+  background-color: #f0f0f0;
+`;
+
+const FormContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  background: #fff;
+  padding: 2rem;
+  border-radius: 8px;
+  height: 300px;
+  box-shadow: 0 4px 6px rgba(0, 0, 0, 0.1);
+`;
+
+const StyledInput = styled.input`
+  padding: 0.5rem;
+  margin-bottom: 1rem;
+  border: 1px solid #ccc;
+  border-radius: 4px;
+  width: 100%;
+  width: 500px;
+`;
+
+const StyledButton = styled.button`
+  padding: 0.5rem 1rem;
+  background-color: #007bff;
+  color: #fff;
+  border: none;
+  border-radius: 4px;
+  cursor: pointer;
+  transition: background-color 0.3s;
+
+  &:hover {
+    background-color: #0056b3;
+  }
+`;
+
 export function App() {
   return (
-    <div>
-
-      <div>
-      <input></input>
-      
-     <button>Shorten url</button>
-      </div>
-    </div>
+    <AppContainer>
+      <FormContainer>
+        <StyledInput placeholder="Enter URL" />
+        <StyledButton>Shorten URL</StyledButton>
+      </FormContainer>
+    </AppContainer>
   );
 }
+
 
 export const router = createBrowserRouter([
   {
