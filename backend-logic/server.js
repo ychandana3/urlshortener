db2662b7a8747b61df113e4195a98aac200c57f2
Yamini Chandana <yaminichandana9@gmail.com>
2024-05-26 01:45:25 +0530

    Fixed cors issue by adding a middleware

--- backend-logic/server.js
+++ backend-logic/server.js
@@ -1,31 +1,32 @@
-
-const express = require('express');
-const { StatusCodes } = require('http-status-codes');
-const urlRoutes = require('./routes/urlRoutes');
-const mongoose = require('mongoose')
-
+const express = require("express");
+const { StatusCodes } = require("http-status-codes");
+const urlRoutes = require("./routes/urlRoutes");
+const mongoose = require("mongoose");
 
 const app = express();
+
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+  );
+  next();
+});
 
 app.use(express.json());
 app.use(urlRoutes);
 
-app.get('/', (req, res) => {
-    res.status(StatusCodes.OK).json({
-        success: true,
-        message: 'server is up and running successfully'
-    });
-});
-
-
 const PORT = 5000;
 
-mongoose.connect("mongodb+srv://chandu:9jJS4bQRmrijgG8D@cluster0.zr8rwnt.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0").then(
-    () => {
-        app.listen(PORT, () => {
-            console.log(`server running on PORT: ${PORT}`);
-        })
-        console.log("Connected")
-    }).catch((err) => console.log(err))
-
-    
+mongoose
+  .connect(
+    "mongodb+srv://chandu:9jJS4bQRmrijgG8D@cluster0.zr8rwnt.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+  )
+  .then(() => {
+    app.listen(PORT, () => {
+      console.log(`server running on PORT: ${PORT}`);
+    });
+    console.log("Connected");
+  })
+  .catch((err) => console.log(err));
